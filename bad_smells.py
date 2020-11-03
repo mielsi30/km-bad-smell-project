cc07c11830c85a77a29fc81bc04b2705e5e0cc26
brenda.ruiz <brenda.ruiz@usi.ch>
2020-11-02 21:09:59 -0600

    Added remaining queries to detect bad smells

--- bad_smells.py
+++ bad_smells.py
@@ -11,17 +11,25 @@ def main():
     print("Detecting bad smells for ontology %s" % file_name)
     detect_bad_smells(file_name)
 
-output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output/queries/")
+output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output/")
+output_path = output_dir + "log.txt"
 
 def detect_bad_smells(file_name):
     get_ontology("file://" + file_name).load()
-    Path(output_path).mkdir(parents=True, exist_ok=True)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
     graph = default_world.as_rdflib_graph()
     long_methods(g=graph)
     long_constructors(g=graph)
+    large_classes(g=graph)
+    method_with_switch(g=graph)
+    constructor_with_switch(g=graph)
+    constructor_long_parameter_list(g=graph)
+    method_long_parameter_list(g=graph)
+    data_class(g=graph)
 
 
 def long_methods(g):
+    print("1. Looking for long methods")
     methods = run_query(
         """SELECT ?cn ?mn ?s (COUNT(*)AS ?tot) WHERE {
                 ?c a tree:ClassDeclaration .
@@ -32,21 +40,18 @@ def long_methods(g):
                 ?m tree:body ?s .
                 ?s a/rdfs:subClassOf* tree:Statement .
             } GROUP BY ?m
-            HAVING (COUNT(?s) >= 10)
+            HAVING (COUNT(?s) >= 20)
         """, g)
 
-    out = open(output_path + "longMethod.txt", "w")
+    out = open(output_path, "w")
     out.write("Long Methods:\n\n")
-
+    out.write("Total: " + str(len(methods)) + "\n")
     for row in methods:
-        print(row)
         out.write("Class: " + row.cn + " Method: " + row.mn + " " + row.tot + " statements\n")
-
     out.close()
-    print("Length = " , len(methods))
-    return methods
 
 def long_constructors(g):
+    print("2. Looking for long constructors")
     constructors = run_query(
         """SELECT ?cn ?con ?s (COUNT(*)AS ?tot) WHERE {
                 ?c a tree:ClassDeclaration .
@@ -60,15 +65,15 @@ def long_constructors(g):
             HAVING (COUNT(?s) >= 20)
         """, g)
 
-    out = open(output_path + "longConstructor.txt", "w")
-    out.write("Long Constructors:\n\n")
+    out = open(output_path, "a")
+    out.write("\nLong Constructors:\n\n")
+    out.write("Total: " + str(len(constructors)) + "\n")
     for row in constructors:
-        print(row)
         out.write("Class: " + row.cn + "  Constructor: " + row.con + row.tot + " statements\n")
-
     out.close()
 
 def large_classes(g):
+    print("3. Looking for large classes")
     classes = run_query(
         """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
                 ?c a tree:ClassDeclaration .
@@ -80,11 +85,140 @@ def large_classes(g):
             HAVING (COUNT(?m) >= 10)
         """, g)
 
-    out = open(output_path + "largeClasses.txt", "w")
-    out.write("Large Classes:\n\n")
+    out = open(output_path, "a")
+    out.write("\nLarge Classes:\n\n")
+    out.write("Total: " + str(len(classes)) + "\n")
     for row in classes:
-        print(row)
-        out.write("Class: " + row.cn + row.tot + " methods\n")
+        out.write("Class: " + row.cn + " " + row.tot + " methods\n")
+
+    out.close()
+
+def method_with_switch(g):
+    print("4. Looking for methods with switch statements")
+    methods = run_query(
+        """SELECT ?mn ?cn ?sw (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?m .
+                ?m a tree:MethodDeclaration .
+                ?m tree:jname ?mn .
+                ?m tree:body ?s .
+                ?sw a tree:SwitchStatement .
+            } GROUP BY ?m
+            HAVING (COUNT(?sw) >= 1)
+        """, g)
+
+    out = open(output_path, "a")
+    out.write("\nMethods with Switch:\n\n")
+    out.write("Total: " + str(len(methods)) + "\n")
+    for row in methods:
+        out.write("Method: " + row.mn + " " + row.tot + " statements\n")
+
+    out.close()
+
+def constructor_with_switch(g):
+    print("5. Looking for constructors with switch statements")
+    methods = run_query(
+         """SELECT ?cn ?con ?s (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?cd .
+                ?cd a tree:ConstructorDeclaration .
+                ?cd tree:jname ?con .
+                ?cd tree:body ?s .
+                ?s a tree:SwitchStatement .
+            } GROUP BY ?cd
+            HAVING (COUNT(?s) >= 1)
+        """, g)
+
+    out = open(output_path, "a")
+    out.write("\nConstructors with Switch:\n\n")
+    out.write("Total: " + str(len(methods)) + "\n")
+    for row in methods:
+        out.write("Constructors: " + row.mn + " " + row.tot + " statements\n")
+
+    out.close()
+
+def constructor_long_parameter_list(g):
+    print("6. Looking for constructors with long parameter list")
+    constructors =  run_query(
+        """SELECT ?cn ?con ?fp (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?m .
+                ?m a tree:ConstructorDeclaration .
+                ?m tree:jname ?con .
+                ?m tree:parameters ?fp .
+                ?fp a tree:FormalParameter .
+            } GROUP BY ?m
+            HAVING (COUNT(?fp) >= 5)
+        """, g)
+
+    out = open(output_path, "a")
+    out.write("\nConstructors with Long Parameter List:\n\n")
+    out.write("Total: " + str(len(constructors)) + "\n")
+    for row in constructors:
+        out.write("Constructors: " + row.mn + " " + row.tot + " parameters\n")
+
+    out.close()
+
+def method_long_parameter_list(g):
+    print("7. Looking for methods with long parameter list")
+    methods =  run_query(
+        """SELECT ?cn ?mn ?fp (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?m .
+                ?m a tree:MethodDeclaration .
+                ?m tree:jname ?mn .
+                ?m tree:parameters ?fp .
+                ?fp a tree:FormalParameter .
+            } GROUP BY ?m
+            HAVING (COUNT(?fp) >= 5)
+        """, g)
+
+    out = open(output_path, "a")
+    out.write("\nMethods with Long Parameter List:\n\n")
+    out.write("Total: " + str(len(methods)) + "\n")
+    for row in methods:
+        out.write("Method: " + row.mn + " " + row.tot + " parameters\n")
+
+    out.close()
+
+
+
+def data_class(g):
+    print("7. Looking for data classes")
+    class_methods = run_query(
+        """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?m .
+                ?m a tree:MethodDeclaration .
+                ?m tree:jname ?mn .
+            } GROUP BY ?cn
+        """, g)
+
+    setters_getters = run_query(
+        """SELECT ?mn ?cn (COUNT(*)AS ?tot) WHERE {
+                ?c a tree:ClassDeclaration .
+                ?c tree:jname ?cn .
+                ?c tree:body ?m .
+                ?m a tree:MethodDeclaration .
+                ?m tree:jname ?mn .
+                FILTER (regex(?mn, "get.*") || regex(?mn, "set.*"))
+            } GROUP BY ?cn
+        """, g)
+
+    out = open(output_path, "a")
+    out.write("\nData Classes:\n\n")
+
+    for row in setters_getters:
+        for method in class_methods:
+            if method.cn == row.cn and row.tot == method.tot:
+                out.write("Total: " + row.tot + "\n")
+                out.write("Class: " + row.cn)
+
 
     out.close()
 
@@ -95,7 +229,8 @@ def run_query(query, g):
     )
     return g.query(q)
 
-# tests not done yet
+
+# TODO: finish tests
 @pytest.fixture
 def setup_ontology():
     onto = owlready2.get_ontology("file://testing.owl").load()
@@ -116,7 +251,7 @@ def setup_ontology():
 
 
 def test_long_method(setup_ontology):
-    # first create a test ontology with  python3 onto_creator.py "testing.owl"
+    # first create a test ontology with python3 onto_creator.py testing.owl
     onto = setup_ontology
     tree = javalang.parse.parse("class A { int f(int x) { x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;x++;}}")
     individ_creator.populate_ontology(onto, tree)
